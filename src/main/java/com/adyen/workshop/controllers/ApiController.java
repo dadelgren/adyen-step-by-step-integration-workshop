6683b59830dd4db15f92ec7103ebc507ad4ea0a4
“Darren <darren@adelgren.co>
2025-12-05 18:54:41 +0000

    Step 14

--- src/main/java/com/adyen/workshop/controllers/ApiController.java
+++ src/main/java/com/adyen/workshop/controllers/ApiController.java
@@ -120,7 +120,43 @@ public class ApiController {
     // Step 14 - Handle Redirect 3DS2 during payment.
     @GetMapping("/handleShopperRedirect")
     public RedirectView redirect(@RequestParam(required = false) String payload, @RequestParam(required = false) String redirectResult) throws IOException, ApiException {
+        var paymentDetailsRequest = new PaymentDetailsRequest();
 
-        return null;
+        PaymentCompletionDetails paymentCompletionDetails = new PaymentCompletionDetails();
+
+        // Handle redirect result or payload
+        if (redirectResult != null && !redirectResult.isEmpty()) {
+            // For redirect, you are redirected to an Adyen domain to complete the 3DS2 challenge
+            // After completing the 3DS2 challenge, you get the redirect result from Adyen in the returnUrl
+            // We then pass on the redirectResult
+            paymentCompletionDetails.redirectResult(redirectResult);
+        } else if (payload != null && !payload.isEmpty()) {
+            paymentCompletionDetails.payload(payload);
+        }
+
+        paymentDetailsRequest.setDetails(paymentCompletionDetails);
+
+        var paymentsDetailsResponse = paymentsApi.paymentsDetails(paymentDetailsRequest);
+        log.info("PaymentsDetailsResponse {}", paymentsDetailsResponse);
+
+        // Handle response and redirect user accordingly
+        var redirectURL = "/result/";
+        switch (paymentsDetailsResponse.getResultCode()) {
+            case AUTHORISED:
+                redirectURL += "success";
+                break;
+            case PENDING:
+            case RECEIVED:
+                redirectURL += "pending";
+                break;
+            case REFUSED:
+                redirectURL += "failed";
+                break;
+            default:
+                redirectURL += "error";
+                break;
+        }
+        return new RedirectView(redirectURL + "?reason=" + paymentsDetailsResponse.getResultCode());
     }
+
 }
